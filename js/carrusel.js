3c26e2f4db090c0baf56c1807e59c077e01ad635
00Matt00 <rodrigo.jara2101@alumnos.ubiobio.cl>
2024-04-28 01:56:35 -0400

    actualizacion 2

--- js/carrusel.js
+++ js/carrusel.js
@@ -16,3 +16,24 @@ punto.forEach ((cadaPunto, i) => {
         punto[i].classList.add('activo')
     })
 })
+
+
+        let isDragging1 = false;
+        let startX1;
+        let scrollLeft1;
+        grande.addEventListener('touchstart', (e) => {
+            isDragging1 = true;
+            startX1 = e.touches[0].clientX;
+            scrollLeft1 = grande.scrollLeft;
+        });
+
+        grande.addEventListener('touchend', () => {
+            isDragging1 = false;
+        });
+
+        grande.addEventListener('touchmove', (e) => {
+            if (!isDragging1) return;
+            const x = e.touches[0].clientX;
+            const walk = (x - startX1) * 1; // Ajusta este valor para controlar la velocidad del desplazamiento
+            carousel1.scrollLeft = scrollLeft1 - walk;
+        });
