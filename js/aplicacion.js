0ec0ab7546a1c5d7ef15f2ae34147451e89df298
Fabián Ferrada <fabianferrada406@gmail.com>
2024-05-02 14:58:42 -0400

    Se implementó el API de registro en profile.html

--- js/aplicacion.js
+++ js/aplicacion.js
@@ -1,93 +1,63 @@
-function obtenerInformacionUsuario(token) {
-    const url = '/usuario';
-    const parametros = {
-        method: 'GET',
-        headers: {
-            'Authorization': `Bearer ${token}`
-        }
-    };
+function obtenerInformacionUsuario (Token) {
+  const url = '/api/usuario'
+  const TokenData = {
+    token: Token
+  }
+  const parametros = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(TokenData)
+  }
 
-    return fetch(url, parametros)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Error al obtener la información del usuario');
-            }
-            return response.json();
-        })
-        .then(data => {
-            return data; // Devuelve la información del usuario
-        })
-        .catch(error => {
-            throw error; // Reenvía el error para manejarlo fuera de la función
-        });
+  return fetch(url, parametros)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Error al obtener la información del usuario');
+      }
+      return response.json();
+    })
+    .then(data => {
+      return data // Devuelve la información del usuario
+    })
+    .catch(error => {
+      throw error // Reenvía el error para manejarlo fuera de la función
+    })
 }
 
-/* Ejemplo de uso
-const token = 'tu_token_de_sesion'; // Reemplaza 'tu_token_de_sesion' con el token real de sesión
- 
-obtenerInformacionUsuario(token)
-  .then(usuario => {
-    console.log('Información del usuario:');
-    console.log('Correo:', usuario.correo);
-    console.log('Nombre de usuario:', usuario.nombreUsuario);
-    console.log('URL de la foto de perfil:', usuario.urlImagen);
-  })
-  .catch(error => {
-    console.error('Error:', error.message);
-  });
-*/
+function crearNuevoUsuario (Correo, NombreUsuario, Contrasena, UrlImagen) {
+  const url = '/api/usuario'
+  const datosUsuario = {
+    correo: Correo,
+    nombreUsuario: NombreUsuario,
+    contrasena: Contrasena,
+    urlImagen: UrlImagen
+  }
+  const parametros = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(datosUsuario)
+  }
 
-function crearNuevoUsuario(correo, nombreUsuario, contrasena, urlImagen) {
-    const url = 'http://34.31.243.232:9000/usuario';
-    const datosUsuario = {
-        correo: correo,
-        nombreUsuario: nombreUsuario,
-        contrasena: contrasena,
-        urlImagen: urlImagen
-    };
-    const parametros = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(datosUsuario)
-    };
+  console.log(url)
 
-    return fetch(url, parametros)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Error al crear el usuario');
-            }
-            return response.json();
-        })
-        .then(data => {
-            return data.exito; // Devuelve true si el registro fue exitoso, false de lo contrario
-        })
-        .catch(error => {
-            throw error; // Reenvía el error para manejarlo fuera de la función
-        });
+  return fetch(url, parametros)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Error al crear el usuario')
+      }
+      return response.json()
+    })
+    .then(data => {
+      return data.exito // Devuelve true si el registro fue exitoso, false de lo contrario
+    })
+    .catch(error => {
+      throw error // Reenvía el error para manejarlo fuera de la función
+    })
 }
-
-/* Ejemplo de uso
-const nuevoUsuario = {
-  correo: 'nuevo_usuario@example.com',
-  nombreUsuario: 'nuevo_usuario',
-  contrasena: 'contraseña_segura',
-  urlImagen: 'https://example.com/imagen_usuario.jpg'
-};
- 
-crearNuevoUsuario(nuevoUsuario.correo, nuevoUsuario.nombreUsuario, nuevoUsuario.contrasena, nuevoUsuario.urlImagen)
-  .then(exito => {
-    if (exito) {
-      console.log('Usuario creado exitosamente');
-    } else {
-      console.log('El usuario ya existe');
-    }
-  })
-  .catch(error => {
-    console.error('Error:', error.message);
-  });
-*/
 
 function editarUsuario(token, correo, nombreUsuario, contrasena, urlImagen) {
     const url = '/usuario';
