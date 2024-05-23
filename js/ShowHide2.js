976d0bb6dfc876f22a02d73092ad9fcf485a446c
00Matt00 <rodrigo.jara2101@alumnos.ubiobio.cl>
2024-05-23 01:46:50 -0400

    new update 2

--- js/ShowHide2.js
+++ js/ShowHide2.js
@@ -1,12 +1,12 @@
 function ShowHide() {
     const boton = document.querySelector("#btnShow2");
-    const miDiv = document.querySelector("#cont-sucur");
+    const miDiv = document.querySelector("#cont-galeria2");
   
     boton.addEventListener("click", function() {
-      if (miDiv.style.height === "42vw") {
-        miDiv.style.height = "123vw";
+      if (miDiv.style.height === "58vw") {
+        miDiv.style.height = "100%";
       } else {
-        miDiv.style.height = "42vw";
+        miDiv.style.height = "58vw";
       }
     });
   }
